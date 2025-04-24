fdb6f0ba660b072d5b4786af91a82bd0a248d523
MomenAhmed600 <momen1616@gmail.com>
2025-04-24 21:33:52 +0200

    update

---
+++ main.js
@@ -0,0 +1,23 @@
+function aboeljdayel() {
+    window.open("https://67409a2b9d6f26a99af6a89d--abo-el-jdayel-project.netlify.app/")
+}
+
+function Youtube() {
+    window.open ("https://momenahmed600.github.io/youtube-project/")
+}
+
+function AllBrands() {
+    window.open("https://github.com/MomenAhmed600/All-Brands-project")
+}
+
+
+// let myprojects = document.querySelector(".projects")
+// let mycontact = document.querySelector(".contact-me")
+
+// myprojects.addEventListener("click", ()=>{
+//     scrollconrainer.scrolldown += 400;
+// });
+
+// function scrolldown() {
+//     window.scrolldown += 300;
+// }
